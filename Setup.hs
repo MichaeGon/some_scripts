2a8c188277a20b1e66219abd486ca6d0633eb6f6
MichaeGon <lodnix@gmail.com>
2016-03-16 17:48:25 +0900

    fix Setup

--- Setup.hs
+++ Setup.hs
@@ -4,6 +4,7 @@ import Distribution.Simple.Setup
 import Distribution.PackageDescription
 import Distribution.ModuleName hiding (main)
 import System.Directory
+import System.FilePath
 import System.Process
 import Control.Monad
 
@@ -11,16 +12,19 @@ main :: IO ()
 main = defaultMainWithHooks simpleUserHooks{buildHook = myBuildHook}
 
 myBuildHook :: PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
-myBuildHook desc _ _ _ = do
+myBuildHook desc lbinfo _ _ = mapM_ (compileExecutable lbinfo) $ executables desc
+
+
+compileExecutable :: LocalBuildInfo -> Executable -> IO ()
+compileExecutable lbinfo exec1 = do
     cxxflags <- readProcess "llvm-config" ["--cxxflags"] []
     ldflags <- readProcess "llvm-config" ["--libdir", "--system-libs", "--libs", "core", "mcjit", "native"] []
-    let exec1 = head . executables $ desc
-        cfiles = cSources . buildInfo $ exec1
+    let cfiles = cSources . buildInfo $ exec1
         f = map toFilePath . otherModules . buildInfo
         hsfiles = f exec1
         ldfacc = foldr (\x acc -> "-l" ++ x ++ " " ++ acc) ("-L" ++ (unwords . lines) ldflags) (extraLibs . buildInfo $ exec1)
-        buildpath = "./dist/build/" ++ exeName exec1
-        tmppath = buildpath ++ "/" ++ exeName exec1 ++ "-tmp"
+        buildpath = buildDir lbinfo </> exeName exec1
+        tmppath = buildpath </> exeName exec1 ++ "-tmp"
         cvt = foldr g ""
         g x acc
             | x == '/' = '_' : acc
@@ -28,15 +32,11 @@ myBuildHook desc _ _ _ = do
     createDirectoryIfMissing True tmppath
     putStrLn "compiling c files..."
     forM_ cfiles $ \x -> do
-        let cmd = "clang++ -c ./" ++ x ++ " -o " ++ tmppath ++ "/" ++ cvt x ++ ".o " ++ cxxflags
+        let cmd = "clang++ -c ./" ++ x ++ " -o " ++ tmppath </> cvt x -<.> "o " ++ cxxflags
         putStrLn cmd
         callCommand cmd
     putStrLn "compiling and linking haskell files..."
-    {-
-    let hscmd = "ghc -c -O --make ./" ++ modulePath exec1 ++ " -odir " ++ tmppath
+    let hscmd = "ghc -O -hidir " ++ tmppath ++ " -odir " ++ tmppath ++ " -tmpdir " ++ tmppath ++ " --make -o " ++ buildpath </> exeName exec1 ++ " Main.hs " ++ foldr (\x acc -> tmppath </> cvt x -<.> "o " ++ acc) ldfacc cfiles
     putStrLn hscmd
     callCommand hscmd
-    putStrLn "linking..."
-    -}
-    callCommand $ "ghc -o --make " ++ buildpath ++ "/" ++ exeName exec1 ++ " Main.hs " ++ foldr (\x acc -> tmppath ++ "/"  ++ cvt x ++ ".o " ++ acc) ldfacc cfiles
     putStrLn "done"
