335e23fef42574388aa9b5941162c7183d26447b
MichaeGon <lodnix@gmail.com>
2016-03-17 11:24:31 +0900

    delete do

--- Setup.hs
+++ Setup.hs
@@ -6,7 +6,6 @@ import Distribution.ModuleName hiding (main)
 import System.Directory
 import System.FilePath
 import System.Process
-import Control.Monad
 
 main :: IO ()
 main = defaultMainWithHooks simpleUserHooks{buildHook = myBuildHook}
@@ -16,27 +15,27 @@ myBuildHook desc lbinfo _ _ = mapM_ (compileExecutable lbinfo) $ executables des
 
 
 compileExecutable :: LocalBuildInfo -> Executable -> IO ()
-compileExecutable lbinfo exec1 = do
-    cxxflags <- readProcess "llvm-config" ["--cxxflags"] []
-    ldflags <- readProcess "llvm-config" ["--libdir", "--system-libs", "--libs", "core", "mcjit", "native"] []
-    let cfiles = cSources . buildInfo $ exec1
-        f = map toFilePath . otherModules . buildInfo
-        hsfiles = f exec1
-        ldfacc = foldr (\x acc -> "-l" ++ x ++ " " ++ acc) ("-L" ++ (unwords . lines) ldflags) (extraLibs . buildInfo $ exec1)
-        buildpath = buildDir lbinfo </> exeName exec1
-        tmppath = buildpath </> exeName exec1 ++ "-tmp"
+compileExecutable lbinfo exec = createDirectoryIfMissing True tmppath
+                            >> putStrLn "compiling c files..."
+                            >> mapM_ cmpcf cfiles
+                            >> putStrLn "compiling and linking haskell files..."
+                            >> hscmd >>= putStrLn
+                            >> hscmd >>= callCommand
+                            >> putStrLn "done"
+    where
+        tmppath = buildpath </> exeName exec ++ "-tmp"
+        buildpath = buildDir lbinfo </> exeName exec
+        cmpcf x = cmd >>= putStrLn >> cmd >>= callCommand
+            where
+                cmd = (("clang++ -c ./" ++ x ++ " -o " ++ tmppath </> cvt x -<.> "o ") ++) <$> cxxflags
+                cxxflags = readProcess "llvm-config" ["--cxxflags"] []
+        cfiles = cSources . buildInfo $ exec
         cvt = foldr g ""
         g x acc
             | x == '/' = '_' : acc
             | otherwise = x : acc
-    createDirectoryIfMissing True tmppath
-    putStrLn "compiling c files..."
-    forM_ cfiles $ \x -> do
-        let cmd = "clang++ -c ./" ++ x ++ " -o " ++ tmppath </> cvt x -<.> "o " ++ cxxflags
-        putStrLn cmd
-        callCommand cmd
-    putStrLn "compiling and linking haskell files..."
-    let hscmd = "ghc -O -hidir " ++ tmppath ++ " -odir " ++ tmppath ++ " -tmpdir " ++ tmppath ++ " --make -o " ++ buildpath </> exeName exec1 ++ " Main.hs " ++ foldr (\x acc -> tmppath </> cvt x -<.> "o " ++ acc) ldfacc cfiles
-    putStrLn hscmd
-    callCommand hscmd
-    putStrLn "done"
+        hscmd = (("ghc -O -hidir " ++ tmppath ++ " -odir " ++ tmppath ++ " -tmpdir " ++ tmppath ++ " --make -o " ++ buildpath </> exeName exec ++ " " ++ modulePath exec ++ " ") ++) <$> ofiles
+            where
+                ofiles = flip (foldr (\x acc -> tmppath </> cvt x -<.> "o " ++ acc)) cfiles <$> ldfacc
+                ldfacc = flip (foldr (\x acc -> "-l" ++ x ++ " " ++ acc)) (extraLibs . buildInfo $ exec) <$> ldflags
+                ldflags = (("-L" ++) . unwords . lines) <$> readProcess "llvm-config" ["--libdir", "--system-libs", "--libs", "core", "mcjit", "native"] []
