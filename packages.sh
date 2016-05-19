d69b1be41ff6a5a738938228939ea9af3cb222b7
MichaeGon <lodnix@gmail.com>
2016-05-19 14:19:15 +0900

    packages

---
+++ packages.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+apm install minimap file-icons highlight-column highlight-line color-picker pigments;
+apm install git-history git-control merge-conflicts git-plus git-log tualo-git-context;
+apm install ide-haskell haskell-ghc-mod ide-haskell-cabal autocomplete-haskell language-haskell haskell-pointfree linter-hlint;
+apm install linter-clang autocomplete-clang;
+apm install language-rust linter-rust racer;
