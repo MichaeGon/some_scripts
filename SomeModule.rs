9869622b4a9f07d26df6d9fa6750dc70cbb20657
MichaeGon <lodnix@gmail.com>
2016-05-28 15:37:03 +0900

    ret

--- SomeModule.rs
+++ SomeModule.rs
@@ -1,5 +1,5 @@
 mod SomeModule {
     macro_rules!getLine{()=>{{let mut s=String::new();std::io::stdin().read_line(&mut s);s}};}
     macro_rules!read{($x:expr)=>{$x.parse().unwrap()};}
-    macro_rules!reads{($x:expr)=>{{for(i, x)in getLine!().trim().split(' ').enumerate(){$x[i]=read!(x);}$x}};}
+    macro_rules!reads{($x:expr)=>{for(i, x)in getLine!().trim().split(' ').enumerate(){$x[i]=read!(x)}};}
 }
