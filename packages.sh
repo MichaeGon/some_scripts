0925dfb96064010ea795351ef0d4f70e1ee42d55
MichaeGon <lodnix@gmail.com>
2016-06-09 22:35:31 +0900

    latex

--- packages.sh
+++ packages.sh
@@ -5,3 +5,4 @@ apm install git-history git-control merge-conflicts git-plus git-log tualo-git-c
 apm install ide-haskell haskell-ghc-mod ide-haskell-cabal autocomplete-haskell language-haskell haskell-pointfree linter-hlint;
 apm install linter-clang autocomplete-clang;
 apm install language-rust linter-rust racer;
+apm install latex pdf-view
