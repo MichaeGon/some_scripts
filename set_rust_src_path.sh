5b37b6860bb7775e8221904c7cc67a2c92271175
MichaeGon <lodnix@gmail.com>
2016-09-30 11:50:47 +0900

    define rust

--- set_rust_src_path.sh
+++ set_rust_src_path.sh
@@ -10,17 +10,28 @@ if [ $# -gt 1 ] ; then
     exit 1
 fi
 
+rust_src_path=`echo $RUST_SRC_PATH`
+
+if [ -z "${rust_src_path}" ] ; then
+    echo "not found RUST_SRC_PATH"
+    exit 1
+fi
+
 page_prefix=https://static.rust-lang.org/dist/
 src_prefix=rustc-
-src_postfix=-src
-src_ext=.tar.gz
+src_ext=-src.tar.gz
 
-src_dir=${src_prefix}${1}${src_postfix}
+src_dir=${src_prefix}${1}
 src_path=${page_prefix}${src_dir}${src_ext}
 
-# paths?
 
 wget ${src_path}
 tar xvf ${src_dir}${src_ext}
 
-# undefined
+rm -rf ${rust_src_path}
+mkdir -p ${rust_src_path}
+
+mv ${src_dir}/src/* ${rust_src_path}
+
+rm ${src_dir}${src_ext}
+rm -rf ${src_dir}
