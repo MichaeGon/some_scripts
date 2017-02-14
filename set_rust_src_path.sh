c0162d8b6451784e0c45577e0f4a41632c20bad4
MichaeGon <lodnix@gmail.com>
2017-02-14 14:47:06 +0900

    fix

--- set_rust_src_path.sh
+++ set_rust_src_path.sh
@@ -12,14 +12,14 @@ fi
 
 page_prefix=https://static.rust-lang.org/dist/
 src_prefix=rustc-
-src_ext=-src.tar.gz
+src_ext=.tar.gz
 
-src_dir=${src_prefix}${rust_ver}
+src_dir=${src_prefix}${rust_ver}-src
 src_path=${page_prefix}${src_dir}${src_ext}
 
 
 wget ${src_path}
-tar xvf ${src_dir}${src_ext}
+tar -xvf ${src_dir}${src_ext}
 
 rm -rf ${rust_src_path}
 mkdir -p ${rust_src_path}
