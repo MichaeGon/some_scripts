3fb9c9d44bd642771b099f960cf527e1ca01b101
MichaeGon <lodnix@gmail.com>
2017-05-11 18:39:35 +0900

    add linters

--- apm_installs.sh
+++ apm_installs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # something
-apm install minimap file-icons highlight-column highlight-line color-picker pigments;
+apm install minimap file-icons highlight-column highlight-line color-picker pigments linter linter-ui-default intentions busy-signal;
 # git
 apm install git-history git-control merge-conflicts git-plus git-log tualo-git-context;
 # haskell
